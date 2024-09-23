828a300c2ecb821e637c93389aa4afbed0065df0
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-23 11:32:10 +0300

    updated: sqlite (ready database usage) - performing operations on the database

--- app/src/main/java/com/ismailmesutmujde/kotlinstoragesqlitereadydatabaseusage/dao/CategoriesDao.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinstoragesqlitereadydatabaseusage/dao/CategoriesDao.kt
@@ -1,4 +1,22 @@
 package com.ismailmesutmujde.kotlinstoragesqlitereadydatabaseusage.dao
 
+import com.ismailmesutmujde.kotlinstoragesqlitereadydatabaseusage.model.Categories
+import com.ismailmesutmujde.kotlinstoragesqlitereadydatabaseusage.sqlite.DatabaseHelper
+
 class CategoriesDao {
+
+    fun allCategories(dbh:DatabaseHelper) : ArrayList<Categories> {
+        val categoryList = ArrayList<Categories>()
+        val db = dbh.writableDatabase
+
+        val cursor = db.rawQuery("SELECT * FROM categories", null)
+
+        while (cursor.moveToNext()) {
+            val category = Categories(cursor.getInt(cursor.getColumnIndexOrThrow("category_id")),
+                cursor.getString(cursor.getColumnIndexOrThrow("category_name")))
+
+            categoryList.add(category)
+        }
+        return categoryList
+    }
 }
--- app/src/main/java/com/ismailmesutmujde/kotlinstoragesqlitereadydatabaseusage/view/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinstoragesqlitereadydatabaseusage/view/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.ismailmesutmujde.kotlinstoragesqlitereadydatabaseusage.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.info.sqlitekullanimihazirveritabani.DatabaseCopyHelper
 import com.ismailmesutmujde.kotlinstoragesqlitereadydatabaseusage.R
+import com.ismailmesutmujde.kotlinstoragesqlitereadydatabaseusage.dao.CategoriesDao
+import com.ismailmesutmujde.kotlinstoragesqlitereadydatabaseusage.sqlite.DatabaseHelper
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +15,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         copyDatabase()
+
+        val db = DatabaseHelper(this)
+
+        val categoryList = CategoriesDao().allCategories(db)
+
+        for(c in categoryList) {
+            Log.e("Category id", (c.category_id).toString())
+            Log.e("Category name", c.category_name)
+        }
 
     }
 
