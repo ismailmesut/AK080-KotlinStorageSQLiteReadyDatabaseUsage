c99f9e21bc2d37c7a61316eaf44ba2666ff89867
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-23 11:54:08 +0300

    updated: sqlite (ready database usage) - table with foreign key

--- app/src/main/java/com/ismailmesutmujde/kotlinstoragesqlitereadydatabaseusage/dao/MoviesDao.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinstoragesqlitereadydatabaseusage/dao/MoviesDao.kt
@@ -1,4 +1,32 @@
 package com.ismailmesutmujde.kotlinstoragesqlitereadydatabaseusage.dao
 
+import com.ismailmesutmujde.kotlinstoragesqlitereadydatabaseusage.model.Categories
+import com.ismailmesutmujde.kotlinstoragesqlitereadydatabaseusage.model.Directors
+import com.ismailmesutmujde.kotlinstoragesqlitereadydatabaseusage.model.Movies
+import com.ismailmesutmujde.kotlinstoragesqlitereadydatabaseusage.sqlite.DatabaseHelper
+
 class MoviesDao {
+
+    fun allMoviesByCategoryId(dbh : DatabaseHelper, category_id : Int) : ArrayList<Movies> {
+        val moviesList = ArrayList<Movies>()
+        val db = dbh.writableDatabase
+        val cursor = db.rawQuery("SELECT * FROM movies,categories,directors WHERE movies.category_id = categories.category_id AND movies.director_id = directors.director_id AND movies.category_id = $category_id", null)
+
+        while (cursor.moveToNext()) {
+            val category = Categories(cursor.getInt(cursor.getColumnIndexOrThrow("category_id")),
+                cursor.getString(cursor.getColumnIndexOrThrow("category_name")))
+
+            val director = Directors(cursor.getInt(cursor.getColumnIndexOrThrow("director_id")),
+                cursor.getString(cursor.getColumnIndexOrThrow("director_name")))
+
+            val movie = Movies(cursor.getInt(cursor.getColumnIndexOrThrow("movie_id")),
+                cursor.getString(cursor.getColumnIndexOrThrow("movie_name")),
+                cursor.getInt(cursor.getColumnIndexOrThrow("movie_year")),
+                cursor.getString(cursor.getColumnIndexOrThrow("movie_poster")),
+                category, director)
+            moviesList.add(movie)
+        }
+
+        return moviesList
+    }
 }
--- app/src/main/java/com/ismailmesutmujde/kotlinstoragesqlitereadydatabaseusage/view/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinstoragesqlitereadydatabaseusage/view/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.info.sqlitekullanimihazirveritabani.DatabaseCopyHelper
 import com.ismailmesutmujde.kotlinstoragesqlitereadydatabaseusage.R
 import com.ismailmesutmujde.kotlinstoragesqlitereadydatabaseusage.dao.CategoriesDao
+import com.ismailmesutmujde.kotlinstoragesqlitereadydatabaseusage.dao.MoviesDao
 import com.ismailmesutmujde.kotlinstoragesqlitereadydatabaseusage.sqlite.DatabaseHelper
 
 
@@ -24,6 +25,21 @@ class MainActivity : AppCompatActivity() {
             Log.e("Category id", (c.category_id).toString())
             Log.e("Category name", c.category_name)
         }
+
+        val movieList = MoviesDao().allMoviesByCategoryId(db, 2)
+
+        for (m in movieList) {
+
+            Log.e("Movie id", (m.movie_id).toString())
+            Log.e("Movie name", m.movie_name)
+            Log.e("Movie year", (m.movie_year).toString())
+            Log.e("Movie poster", m.movie_poster)
+            Log.e("Movie category id", (m.category.category_id).toString())
+            Log.e("Movie category name", m.category.category_name)
+            Log.e("Movie director id", (m.director.director_id).toString())
+            Log.e("Movie director name", m.director.director_name)
+        }
+
 
     }
 
